2e416fd8c632114f6698c0201a81c8448efbb41c
sounishnath003 <flock.sinasini@gmail.com>
2020-09-19 22:27:54 +0530

    backend sqlite3 added for simplicity

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,23 +4,31 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () =>
+      import('./home/home.module').then((m) => m.HomePageModule),
   },
   {
     path: 'message/:id',
-    loadChildren: () => import('./view-message/view-message.module').then( m => m.ViewMessagePageModule)
+    loadChildren: () =>
+      import('./view-message/view-message.module').then(
+        (m) => m.ViewMessagePageModule
+      ),
   },
   {
     path: '',
     redirectTo: 'home',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
+  {
+    path: 'newproduct',
+    loadChildren: () => import('./newproduct/newproduct.module').then( m => m.NewproductPageModule)
+  }
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
