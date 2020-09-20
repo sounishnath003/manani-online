625bc8dabd3beb4a5117036f7705d625abf014ef
sounishnath003 <flock.sinasini@gmail.com>
2020-09-20 11:43:04 +0530

    cors added | backend ready

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const cors = require('cors')
 const app = express();
 const port = process.env.PORT || 5000;
 // database setup
 const db = require("./database");
 db.setupDB();
 
+// cors eneable
+app.use(cors())
+
 // Product Routes
 app.use("/products", require("./routes/product"));
 
-app.listen(port, () => console.log(`listening on http://localhost:${port}`));
+app.listen(port, () => console.log(`listening on http://localhost:${port}/products`));
